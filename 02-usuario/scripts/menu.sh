e046b6d71d2c859b13d712d8a623febfab4873c9
Max Eder <maxeder87@gmail.com>
2023-12-01 21:10:15 -0300

    Archivos modificados el 1 de diciemnbre de 2023

--- 02-usuario/scripts/menu.sh
+++ 02-usuario/scripts/menu.sh
@@ -1,6 +1,16 @@
 PS3='Elija>'
 
-echo "Hola, soy el menu del tp. Que quiere hacer?"
-select opcion in "Clasificar archivos" "Monitorear Sistema" "Analizar Texto" "Salir"
+echo "Hola, soy el menu del TP. Que quiere hacer?"
+select opcion in "Clasificar archivos" "Monitorear Sistema" "Analizar Textos" "Salir"
 do
-[ -z $opcion ] && echo "Elegir opcion valida 
+
+[ -z $opcion ] && echo "Elegir opcion valida." && continue
+[ $REPLY == 1 ] && clasificar && continue
+[ $REPLY == 2 ] && echo "Elijo Monitorear el Sistema" && continue
+[ $REPLY == 3 ] && echo "Analizar Texto" && continue
+[ $REPLY == 4 ] && echo "Salir" && break
+echo "Opcion elegida:  "  $opcion
+
+done
+
+exit 0
